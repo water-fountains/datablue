87d3b3b5b56877dd2e4d8913e84b0618c743be88
mmmatthew <matthew.mdv@gmail.com>
2018-07-21 21:02:33 +0200

    remove unused function

--- server/api/services/conflate.data.service.js
+++ server/api/services/conflate.data.service.js
@@ -6,39 +6,6 @@ const turf = require('@turf/distance');
 const md5 = require('js-md5');
 
 // This service combines data from osm and wikidata
-
-export function combineData(r) {
-  // simple function for combining data from wikidata and osm in a basic way: only panorama url is copied over
-  return new Promise((resolve, reject) => {
-    let wikidata = r[0];
-    let osm = r[1];
-    
-    // copy some properties over to osm
-    if(typeof wikidata.pano_url.value !== 'undefined'){
-      osm['pano_url'] = wikidata.pano_url;
-    }else{
-      // use default panorama link
-      osm['pano_url'] = {
-        value: `https://www.instantstreetview.com/@${osm.coords.value[1]},${osm.coords.value[0]},0h,0p,1z`
-      }
-    }
-    // format data as a geojson feature
-    let data_geojson = {
-      geometry:{
-        type: 'Point',
-        coordinates: osm.coords.value
-      },
-      properties: {}
-    };
-    Object.keys(osm).forEach(key=>{
-      data_geojson.properties[key] = osm[key].value;
-    });
-    // return the translated data
-    resolve(data_geojson);
-    // if there is an issue, reject the promise
-    setTimeout(() => reject('woops'), 500);
-  })
-}
 export function conflate(r) {
   return new Promise((resolve, reject)=>{
     let conflated_fountains = [];
