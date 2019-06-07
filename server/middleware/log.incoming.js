338a459ae95daef333691b03d7944932de7e779d
mmmatthew <matthew.mdv@gmail.com>
2019-06-07 15:01:49 +0200

    remove request id from logger info

--- server/middleware/log.incoming.js
+++ server/middleware/log.incoming.js
@@ -2,7 +2,7 @@
 function logIncomingRequests(logger) {
   return (req, res, next) => {
     // without custom serializers, we must be explicit
-    logger.info(`incoming request: ${req.url} Request id: ${req.id}`, 'Incoming request');
+    logger.info(`incoming request: ${req.url}`, 'Incoming request');
     next();
   }
 }
