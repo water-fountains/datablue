6795ebedce92b9876db01897172db4edd8e084e7
Ralf Hauser <hauser@privasphere.com>
2020-03-21 20:24:45 +0100

    https://github.com/water-fountains/datablue/issues/47

--- config/fountain.properties.js
+++ config/fountain.properties.js
@@ -1,8 +1,13 @@
 /*
  * @license
- * (c) Copyright 2019 | MY-D Foundation | Created by Matthew Moy de Vitry
+ * (c) Copyright 2019 -2020 | MY-D Foundation | Created by Matthew Moy de Vitry
  * Use of this code is governed by the GNU Affero General Public License (https://www.gnu.org/licenses/agpl-3.0)
  * and the profit contribution agreement available at https://www.my-d.org/ProfitContributionAgreement
+ * 
+ * Each time you change the this file, you need to run
+ *     
+ *   ~/git/proximap$ npm run fountain_properties * 
+ * 
  */
 
 import {PROP_STATUS_OK, PROP_STATUS_WARNING} from "../server/common/constants";
