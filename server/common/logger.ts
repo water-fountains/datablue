188749e8bfbd14a0d78706c65681264ca5869881
Robert Stoll <robert.stoll@tegonal.com>
2021-03-10 15:08:20 +0100

    remove pino and timberio as own logger is used

--- server/common/logger.ts
+++ server/common/logger.ts
@@ -5,26 +5,6 @@
  * and the profit contribution agreement available at https://www.my-d.org/ProfitContributionAgreement
  * 
  */
-// import pino from "pino";
-// import { Timber } from "@timberio/node";
-
-
-//TODO not used at all as it seems, only for this one log message, so commenting out for the moment
-// let logger;
-// // Use timber if information is provided
-// if(process.env.TIMBER_KEY && process.env.TIMBER_SOURCE_ID){
-//     //	https://github.com/timberio/timber-js/issues/74 RFE for file name and line numbers
-//     logger = new Timber(process.env.TIMBER_KEY, process.env.TIMBER_SOURCE_ID);
-//     logger.info('logger.js: Timber logger ' +new Date().toISOString());
-// } else { 
-//   // https://github.com/pinojs/pino/issues/687  RFE for file name and line numbers
-//   logger = pino({
-//     name: process.env.APP_ID,
-//     level: process.env.LOG_LEVEL,
-//   });
-//   logger.info('logger.js: pino logger '+new Date().toISOString());
-// }
-
 export class CustomLogger {
    getCaller() {
          let calr = 'callerNotFound';
